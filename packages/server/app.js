9a3f73e1e96775aea8f0bed35dee65b98de989b5
Wesley Ford <wes@wesdev.pro>
2022-07-19 12:34:13 -0400

    Added Temp Endpoint

--- packages/server/app.js
+++ packages/server/app.js
@@ -1,8 +1,7 @@
 import dotenv from 'dotenv'
 import express from 'express'
+var app = express()
 dotenv.config()
-const test = process.env.HELLO_WORLD
-const app = express()
 
 //setup middleware for JSON
 app.use(express.json())
@@ -11,6 +10,54 @@ app.use(express.json())
 app.get('/', (req, res) => {
     // Return json 
     res.send({"myMessage": test})
+})
+
+app.get("/v1/stats/:userSentIdentification", (req, res) => {
+    res.send({ 
+        "uuid": req.params.userSentIdentification,
+        "name": {
+            "first": "Donald",
+            "Last": "Trump"
+        },
+        "currentGpa": 4.0,
+        "predictedGpa": 4.0,
+        "gpaGoal": 3.6,
+        "classes": [
+            {
+                "name": "History of Civ",
+                "grade": 100,
+                "creditHours": 4,
+                "assignmentTypes": [
+                    {
+                        "name": "Quizes",
+                        "weight": 0.20,
+                         "id": 1
+                    },
+                    {
+                        "name": "Test and Exams",
+                        "weight": 0.30,
+                        "id": 2
+                    },
+                    {
+                        "name": "Final Exam",
+                        "weight": 50,
+                        "id": 3
+                    }
+                ],
+                "asignments": [
+                    {
+                        "name": "Syllabus Review",
+                        "grade": 100,
+                        "id": 2
+                    },
+                    {
+                        "name": "Homework1",
+                        "grade": 100
+                    }
+                ],
+            }
+        ]
+    })
 })
 
 app.listen(process.env.PORT || 8080, () => {
